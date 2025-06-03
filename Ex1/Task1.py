5ee678389e326891c4828f2966bd6be6e59d53fc
Maciej Szpak <mszpak21@gmail.com>
2025-06-03 23:27:56 +0200

    Added main script and browser automation class

---
+++ Ex1/Task1.py
@@ -0,0 +1,106 @@
+from selenium import webdriver
+from selenium.common import NoSuchElementException
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver import ActionChains
+
+class AutomateBrowser:
+
+    def __init__(self):
+        options = Options()
+        #options.headless = True
+        self.driver = webdriver.Firefox(options=options)
+        self.wait = WebDriverWait(self.driver, 10)
+        self.actions = ActionChains(self.driver)
+        self.current_url = "https://www.telerik.com/kendo-react-ui/components/layout/contextmenu"
+
+    def Open_URL(self):
+        try:
+            self.driver.get(self.current_url)
+            print('Program running...')
+            print('\nOpening the url: {}'.format(self.current_url))
+        except Exception as e:
+            print('Something went wrong: {}'.format(e))
+
+    def Reject_Cookies(self):
+        try:
+            reject = self.wait.until(
+                EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Reject All')]"))
+            )
+            self.driver.execute_script('arguments[0].click();', reject)
+            print('Cookies rejected successfully.')
+        except NoSuchElementException:
+            print('Cookies not found.')
+
+    def Swap_To_Iframe(self):
+        try:
+            iframe = self.wait.until(
+                EC.presence_of_element_located((By.XPATH, "//iframe[contains(@class, 'demo-module--demoFrame')]"))
+            )
+            self.driver.switch_to.frame(iframe)
+        except NoSuchElementException:
+            print('No iframe found.')
+
+    def Default_Frame(self):
+        try:
+            self.driver.switch_to.default_content()
+        except NoSuchElementException:
+            print('Default frame actually enabled.')
+
+    def Scroll_Down(self):
+        try:
+            example_element = self.wait.until(
+                EC.presence_of_element_located((By.XPATH, "//*[@class='k-body']"))
+            )
+            self.driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", example_element)
+            print("Scrolled down successfully!")
+        except NoSuchElementException:
+            print('Example element not found.')
+        except Exception as e:
+            print('Error scrolling down: {}'.format(e))
+
+    def Change_Theme(self):
+        try:
+            self.wait.until(
+                EC.element_to_be_clickable((By.XPATH, "//button[@aria-label='Change theme']"))
+            ).click()
+            self.wait.until(
+                EC.element_to_be_clickable((By.XPATH, "//button[13]"))  # achieved by a browser extension SelectorsHub
+            ).click()
+            print("Successfully changed Material Theme to Main!")
+        except NoSuchElementException:
+            print('Dropdown button not found.')
+        except Exception as e:
+            print('Error clicking material theme: {}'.format(e))
+
+    def Style_Underline(self):
+        try:
+            rc_button = self.wait.until(
+                EC.presence_of_element_located((By.XPATH, "//div[@class='target']"))
+            )
+            self.actions.context_click(rc_button).perform()
+            style_btn = self.wait.until(
+                EC.element_to_be_clickable((By.XPATH, "//*[text()='Style']"))
+            )
+            self.actions.move_to_element(style_btn).perform()
+            self.wait.until(
+                EC.element_to_be_clickable((By.XPATH, "//*[text()='Underline']"))
+            ).click()
+            print("Clicked underline button!")
+        except Exception as e:
+            print('Error changing button style: {}'.format(e))
+
+    def Take_Screenshot(self):
+        try:
+            self.driver.get_screenshot_as_file('whole_website.png')
+            print("Successfully taken screenshot!")
+        except Exception as e:
+            print('Error taking screenshot: {}'.format(e))
+
+    def Driver_quit(self):
+        try:
+            self.driver.quit()
+        except Exception as e:
+            print('Error quiting driver: {}'.format(e))
--- Ex1/main.py
+++ Ex1/main.py
@@ -1,106 +1,19 @@
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.firefox.service import Service
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver import ActionChains
-import time
+from Task1 import AutomateBrowser as AB
 
-options = Options()
-options.add_argument('-start-maximized')
-#options.add_argument('--headless')
+def RunBrowser():
+    auto = AB()
+    auto.Open_URL()
+    auto.Reject_Cookies()
+    auto.Swap_To_Iframe()
+    auto.Scroll_Down()
+    auto.Default_Frame()
+    auto.Change_Theme()
+    auto.Swap_To_Iframe()
+    auto.Style_Underline()
+    auto.Default_Frame()
+    auto.Take_Screenshot()
+    input('Press Enter to continue...')
+    auto.Driver_quit()
 
-options.binary_location = "C:\\Program Files\\Mozilla Firefox\\firefox.exe" # pass your own firefox.exe path
-service = Service('C:\\Users\\mszpa\\geckodriver.exe') # pass your own geckodriver.exe path
-
-driver = webdriver.Firefox(service=service, options=options)
-wait = WebDriverWait(driver, 10)
-
-try:
-    driver.get("https://www.telerik.com/kendo-react-ui/components/layout/contextmenu")
-    iframe = wait.until(
-        EC.presence_of_element_located((By.XPATH, "//iframe[contains(@class, 'demo-module--demoFrame')]"))
-    )
-    actions = ActionChains(driver)
-
-    try:
-        reject_cookie = wait.until(
-            EC.element_to_be_clickable((By.XPATH, "//button[text()='Reject All']"))
-        )
-        driver.execute_script("arguments[0].click();", reject_cookie)
-        print("Rejected cookies successfully!")
-    except Exception as e:
-        print("Error rejecting cookies: ", e)
-
-    try:
-        driver.switch_to.frame(iframe)
-        print("Successfully switched to iframe!")
-    except Exception as e:
-        print("Error finding iframe: ", e)
-
-    try:
-        example_element = wait.until(
-            EC.presence_of_element_located((By.XPATH, "//*[@class='k-body']"))
-        )
-        driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", example_element)
-        print("Scrolled down successfully!")
-        driver.switch_to.default_content()
-        print("Switched to default content!")
-    except Exception as e:
-        print("Error scrolling down: ", e)
-
-    try:
-        dropdown_btn = wait.until(
-            EC.element_to_be_clickable((By.XPATH, "//button[@aria-label='Change theme']"))
-        )
-        dropdown_btn.click()
-        print("Successfully clicked dropdown button!")
-    except Exception as e:
-        print("Error clicking dropdown button: ", e)
-
-    try:
-        mat_theme_option = wait.until(
-            EC.element_to_be_clickable((By.XPATH, "//button[13]")) # achieved by a browser extension SelectorsHub
-        )
-        mat_theme_option.click()
-        print("Successfully clicked mat theme option!")
-    except Exception as e:
-        print("Error clicking mat theme option: ", e)
-
-    try:
-        driver.switch_to.frame(iframe)
-        right_click_btn = wait.until(
-            EC.element_to_be_clickable((By.XPATH, "//div[@class='target']"))
-        )
-        actions.context_click(right_click_btn).perform()
-        print("Successfully right clicked the button!")
-    except Exception as e:
-        print("Error right clicking the button: ", e)
-
-    try:
-        style_btn = wait.until(
-            EC.element_to_be_clickable((By.XPATH, "//*[text()='Style']"))
-        )
-        actions.move_to_element(style_btn).perform()
-        underline_btn = wait.until(
-            EC.element_to_be_clickable((By.XPATH, "//*[text()='Underline']"))
-        )
-        underline_btn.click()
-        print("Clicked underline button!")
-    except Exception as e:
-        print("Error clicking style: ", e)
-
-    time.sleep(1)
-
-    try:
-        driver.get_screenshot_as_file("whole_website.png")
-        print("Captured and saved screenshot!")
-    except Exception as e:
-        print("Error getting screenshot: ", e)
-
-    input("Press enter to continue...")
-except Exception as e:
-    print(e)
-finally:
-    driver.quit()
+if __name__ == '__main__':
+    RunBrowser()
