d7fe8a487a0d49eba511d851113cdeccaf7476e1
Maciej Szpak <mszpak21@gmail.com>
2025-06-05 16:51:34 +0200

    Changed names of definitions to lowercase

--- Ex1/Task1.py
+++ Ex1/Task1.py
@@ -1,11 +1,9 @@
-import os
 import time
-
 from selenium import webdriver
 from selenium.common import NoSuchElementException
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.firefox import GeckoDriverManager
@@ -22,7 +20,7 @@ class AutomateBrowser:
         self.actions = ActionChains(self.driver)
         self.current_url = "https://www.telerik.com/kendo-react-ui/components/layout/contextmenu"
 
-    def Open_URL(self):
+    def open_url(self):
         try:
             self.driver.get(self.current_url)
             print('Program running...')
@@ -30,35 +28,35 @@ class AutomateBrowser:
         except Exception as e:
             print('Something went wrong: {}'.format(e))
 
-    def Reject_Cookies(self):
+    def reject_cookies(self):
         try:
             reject = self.wait.until(
-                EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Reject All')]"))
+                ec.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Reject All')]"))
             )
             self.driver.execute_script('arguments[0].click();', reject)
             print('Cookies rejected successfully.')
         except NoSuchElementException:
             print('Cookies not found.')
 
-    def Swap_To_Iframe(self):
+    def swap_to_iframe(self):
         try:
             iframe = self.wait.until(
-                EC.presence_of_element_located((By.XPATH, "//iframe[contains(@class, 'demo-module--demoFrame')]"))
+                ec.presence_of_element_located((By.XPATH, "//iframe[contains(@class, 'demo-module--demoFrame')]"))
             )
             self.driver.switch_to.frame(iframe)
         except NoSuchElementException:
             print('No iframe found.')
 
-    def Default_Frame(self):
+    def default_frame(self):
         try:
             self.driver.switch_to.default_content()
         except NoSuchElementException:
             print('Default frame actually enabled.')
 
-    def Scroll_Down(self):
+    def scroll_down(self):
         try:
             example_element = self.wait.until(
-                EC.presence_of_element_located((By.XPATH, "//*[@class='k-body']"))
+                ec.presence_of_element_located((By.XPATH, "//*[@class='k-body']"))
             )
             self.driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", example_element)
             print("Scrolled down successfully!")
@@ -67,13 +65,13 @@ class AutomateBrowser:
         except Exception as e:
             print('Error scrolling down: {}'.format(e))
 
-    def Change_Theme(self):
+    def change_theme(self):
         try:
             self.wait.until(
-                EC.element_to_be_clickable((By.XPATH, "//button[@aria-label='Change theme']"))
+                ec.element_to_be_clickable((By.XPATH, "//button[@aria-label='Change theme']"))
             ).click()
             self.wait.until(
-                EC.element_to_be_clickable((By.XPATH, "//button[13]"))  # achieved by a browser extension SelectorsHub
+                ec.element_to_be_clickable((By.XPATH, "//button[13]"))  # achieved by a browser extension SelectorsHub
             ).click()
             print("Successfully changed Material Theme to Main!")
         except NoSuchElementException:
@@ -81,36 +79,36 @@ class AutomateBrowser:
         except Exception as e:
             print('Error clicking material theme: {}'.format(e))
 
-    def Style_Underline(self):
+    def style_underline(self):
         try:
             rc_button = self.wait.until(
-                EC.presence_of_element_located((By.XPATH, "//div[@class='target']"))
+                ec.presence_of_element_located((By.XPATH, "//div[@class='target']"))
             )
             self.actions.context_click(rc_button).perform()
             style_btn = self.wait.until(
-                EC.element_to_be_clickable((By.XPATH, "//*[text()='Style']"))
+                ec.element_to_be_clickable((By.XPATH, "//*[text()='Style']"))
             )
             self.actions.move_to_element(style_btn).perform()
             self.wait.until(
-                EC.element_to_be_clickable((By.XPATH, "//*[text()='Underline']"))
+                ec.element_to_be_clickable((By.XPATH, "//*[text()='Underline']"))
             ).click()
             print("Clicked underline button!")
         except Exception as e:
             print('Error changing button style: {}'.format(e))
 
-    def Take_Screenshot(self):
+    def take_screenshot(self):
         try:
             time.sleep(1)
             screenshot = self.driver.save_screenshot('whole_website.png')
 
             if screenshot:
-                print("Successfully taken screenshot! {}")
+                print("Successfully taken screenshot!")
             else:
                 print('Could not take screenshot!')
         except Exception as e:
             print('Error taking screenshot: {}'.format(e))
 
-    def Driver_quit(self):
+    def driver_quit(self):
         try:
             self.driver.quit()
             print('Driver closed successfully.')
--- Ex1/main.py
+++ Ex1/main.py
@@ -1,21 +1,21 @@
 import time
-from Task1 import AutomateBrowser as AB
+from Task1 import AutomateBrowser as ab
 
 def RunBrowser():
-    auto = AB()
-    auto.Open_URL()
-    auto.Reject_Cookies()
-    auto.Swap_To_Iframe()
-    auto.Scroll_Down()
-    auto.Default_Frame()
-    auto.Change_Theme()
-    auto.Swap_To_Iframe()
-    auto.Style_Underline()
-    auto.Default_Frame()
-    auto.Take_Screenshot()
+    auto = ab()
+    auto.open_url()
+    auto.reject_cookies()
+    auto.swap_to_iframe()
+    auto.scroll_down()
+    auto.default_frame()
+    auto.change_theme()
+    auto.swap_to_iframe()
+    auto.style_underline()
+    auto.default_frame()
+    auto.take_screenshot()
     #input('Press Enter to continue...')
     time.sleep(2)
-    auto.Driver_quit()
+    auto.driver_quit()
 
 if __name__ == '__main__':
     RunBrowser()
